0dad092beea1a6027c218aa6fc3229775397185b
SAJEB KHAN <sajebk007@gmail.com>
2025-11-22 18:06:57 +0530

    exception handler

--- src/main/java/com/bookmyshow/bms/dto/BookingDto.java
+++ src/main/java/com/bookmyshow/bms/dto/BookingDto.java
@@ -1,8 +1,15 @@
 package com.bookmyshow.bms.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookingDto {
     private Long id;
     private String bookingNumber;
---
+++ src/main/java/com/bookmyshow/bms/exception/SeatUnavailableException.java
@@ -0,0 +1,8 @@
+package com.bookmyshow.bms.exception;
+
+public class SeatUnavailableException extends RuntimeException{
+
+    public SeatUnavailableException(String message){
+        super(message);
+    }
+}
